8b649ed74d99b9202100d6c47e76f7445aebb6ff
Abdenour Mabed [m1] <n.peugnet@free.fr>
2020-02-28 11:42:25 +0100

    finish tme4

---
+++ ioc20_4_mabed_peugnet/sketch_mabed_peugnet/ioc20_4_mabed_peugnet.md
@@ -0,0 +1,14 @@
+# TP4 : Programmation Arduino 
+
+## Questions :
+* WaitForTime est un tableau de timers, un pour chaque tâche périodique.
+
+* Même si deux tâches ont la même période, elles s'éxecutent séparément, à des dates précises. Celà grace au timers atttribués à chacunes des tâches.
+
+* Le nom de la boîte à lettre est "mb0"; state est initialisé à 'EMPTY', tandis que val n'est pas initialisé.
+
+
+
+![schéma](./graphviz.png)
+
+
--- ioc20_4_mabed_peugnet/sketch_mabed_peugnet/sketch_mabed_peugnet.ino
+++ ioc20_4_mabed_peugnet/sketch_mabed_peugnet/sketch_mabed_peugnet.ino
@@ -9,36 +9,12 @@
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+
+#define OLED_RESET     4                // Reset pin 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-#define NUMFLAKES     10 // Number of snowflakes in the animation example
 
-#define LOGO_HEIGHT   16
-#define LOGO_WIDTH    16
-
-
-enum {EMPTY, FULL};
-struct mailbox {
-  int state;
-  int val;
-} mb0 = {.state = EMPTY};
-
-void loop_T1(&mb) {
-  if (mb->state != EMPTY) return; // attend que la mailbox soit vide
-  mb->val = 42;
-  mb->state = FULL;
-}
-
-void loop_T2(&mb) {
-  if (mb->state != FULL) return; // attend que la mailbox soit pleine
-  // usage de mb->val
-  mb->state = EMPTY;
-}
-
-
-
-#define MAX_WAIT_FOR_TIMER 4
+#define MAX_WAIT_FOR_TIMER 5
 unsigned int waitFor(int timer, unsigned long period){
   static unsigned long waitForTimer[MAX_WAIT_FOR_TIMER];  // il y a autant de timers que de tâches périodiques
   unsigned long newTime = micros() / period;              // numéro de la période modulo 2^32 
@@ -48,10 +24,30 @@ unsigned int waitFor(int timer, unsigned long period){
   return delta;
 }
 
-//--------- définition de la tache Led
+//----------------------------------Boîtes à lettres-------------------------------------//
+
+enum {EMPTY, FULL};
+
+struct mailbox0 {                                          
+  int state;                                              // valeur indiquant l'état de la boite
+  int val;                                                // valeur pouvant être lue ou écrite par deux tâches indépendantes l'une de l'autre.                                           
+} mb0 = {.state = EMPTY};
+
+
+struct mailbox2 {                                          
+  int state;                                              // valeur indiquant l'état de la boite
+  int val;                                                // valeur pouvant être lue ou écrite par deux tâches indépendantes l'une de l'autre.                                           
+} mb2 = {.state = EMPTY};
+
+struct mailbox1{
+  int state;
+  int val;
+}mb1 = {.state = EMPTY};
+
+//----------------------------------Définition de la tâche Led-------------------------//
 
 struct Led_st {
-  int timer;                                              // numéro du timer pour cette tâche utilisé par WaitFor
+  int timer;                                              // numéro du timer pour cette tâche utilisée par WaitFor
   unsigned long period;                                   // periode de clignotement
   int pin;                                                // numéro de la broche sur laquelle est la LED
   int etat;                                               // etat interne de la led
@@ -66,17 +62,31 @@ void setup_Led( struct Led_st * ctx, int timer, unsigned long period, byte pin) 
   digitalWrite(pin, ctx->etat);
 }
 
-void loop_Led(struct Led_st * ctx) {
-  if (!waitFor(ctx->timer, ctx->period)) return;          // sort s'il y a moins d'une période écoulée
-  digitalWrite(ctx->pin,ctx->etat);                       // ecriture
-  ctx->etat = 1 - ctx->etat;                              // changement d'état
+void loop_Led(struct Led_st * ctx, struct mailbox0 *mb, struct mailbox1 *mb1) {
+  if (!waitFor(ctx->timer, ctx->period)) return;                                 // sort s'il y a moins d'une période écoulée
+  //digitalWrite(ctx->pin,ctx->etat);                                            // écriture
+  //ctx->etat = 1 - ctx->etat;                                                   // changement d'état
+  
+  if (mb->state != FULL) return;                          // attend que la mailbox soit pleine 
+  int valeur=map(mb->val,0,1023,100,750);                 //On lit la valeur relevée par la photorésistance et l'adopte à notre besoin de faire clignoter la Led avec map
+  digitalWrite(ctx->pin,HIGH);                            // la Led clignote de façon inversement proportionnel à la lumière reçue.
+  delay(valeur);                                          //Plus la lumière est intense plus le delais de clignotement de la led est long et donc clignotera lentement.
+  digitalWrite(ctx->pin,LOW);                             
+  delay(valeur);
+  
+  if (mb1->state != FULL) return;
+  //if (mb1->val != HIGH){
+    for(int i=0;i<10000;i++)
+    digitalWrite(ctx->pin,mb1->val);
+  
 }
 
-//--------- definition de la tache Mess
+
+//-------------------------------Définition de la tâche Mess-------------------------------//
 
 struct Mess_st {
   int timer;                                              // numéro de timer utilisé par WaitFor
-  unsigned long period;                                             // periode d'affichage
+  unsigned long period;                                   // periode d'affichage
   char mess[20];
 } Mess_t ; 
 
@@ -88,16 +98,14 @@ void setup_Mess(struct Mess_st * ctx, int timer, unsigned long period, const cha
 }
 
 void loop_Mess(struct Mess_st *ctx) {
-  int x;
-  if (!(x=waitFor(ctx->timer,ctx->period))) return;         // sort s'il y a moins d'une période écoulée
-  Serial.println(x);
+  if (!(waitFor(ctx->timer,ctx->period))) return;         // sort s'il y a moins d'une période écoulée
   Serial.println(ctx->mess);                              // affichage du message
 }
 
-//--------- definition de la tache Oled
+//-------------------------------Définition de la tache Oled---------------------------------//
 
 struct Oled_st {
-  int timer;                                              // numéro du timer pour cette tâche utilisé par WaitFor
+  int timer;                                              // numéro du timer pour cette tâche utilisée par WaitFor
   unsigned long period;                                                                                
 } Oled_t; 
 
@@ -105,94 +113,134 @@ void setup_Oled(struct Oled_st * ctx, int timer, unsigned long period) {
   ctx->timer = timer;
   ctx->period = period;
   
+//Initialisation de l'écran Oled  
   Serial.begin(9600);
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+    { // Address 0x3C for 128x32
+      Serial.println(F("SSD1306 allocation failed"));
+      for(;;); // Don't proceed, loop forever
+    }
     
-    // Show initial display buffer contents on the screen --
   // the library initializes this with an Adafruit splash screen.
   display.display();
-  delay(2000); // Pause for 2 seconds
 
-  display.setTextSize(1);      // Normal 1:1 pixel scale
-  display.setTextColor(SSD1306_WHITE); // Draw white text
-  display.setCursor(0,0);     // Start at top-left corner
-  display.cp437(true);         // Use full 256 char 'Code Page 437' font
-  display.clearDisplay();
-
-  //testscrolltext();    // Draw scrolling text
-  
- }
-int cpt =0;
-
-void loop_Oled(struct Oled_st *ctx) {
-  
-  if (!(waitFor(ctx->timer,ctx->period))) return;         // sort s'il y a moins d'une période écoulée
- testscrolltext(); 
- 
+  display.setTextSize(1);                                 // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE);                    // Draw white text
+  display.setCursor(0,0);                                 // Start at top-left corner
+  display.cp437(true);                                    // Use full 256 char 'Code Page 437' font
+  display.clearDisplay();   
 }
 
 
-//--------- Déclaration des tâches
+int cpt =0;                                               //Initialisation de notre compteur
+
+void loop_Oled(struct Oled_st * ctx, struct mailbox0 *mb) {
+  if (!(waitFor(ctx->timer,ctx->period))) return;         // sort s'il y a moins d'une période écoulée
+  display.clearDisplay();
+  display.setTextSize(2); 
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+
+  if (mb->state != FULL) return;                          //On attend que la boîte soit pleine
+  int valeur=map(mb->val,0,1023,0,100);                   //On lit la valeur relevée par la photorésistance et l'adopte à notre besoin de l'afficher en pourcentage avec map 
+  display.print(valeur);                                  //Affichage du pourcentage de luminosité reçu par la photorésistance
+  display.println("%"); 
+  mb->state = EMPTY;
+  display.display();
+  
+  /*display.println(F("Compteur:"));                      //Affichage du compteur
+  display.display();
+  cpt++;                                                  //Incrémentation du compteur
+  display.print(cpt);
+  display.display(); */ 
+}
+
+
+
+//-----------------------------Définition de la tâche Lum---------------------------------//
+
+struct Lum_st {
+  int timer;                                              // numéro du timer pour cette tâche utilisé par WaitFor
+  unsigned long period;                                                                                
+}; 
+
+void setup_Lum(struct Lum_st * ctx, int timer, unsigned long period) {
+  ctx->timer = timer;
+  ctx->period = period;
+}
+
+void loop_Lum(struct Lum_st * ctx, struct mailbox0 *mb0, struct mailbox2 *mb2) {
+  if (!(waitFor(ctx->timer,ctx->period))) return;
+  int valeur = analogRead(A1);                                        //lit le le port analogique A1
+  
+  if (mb0->state != EMPTY) return;                                     // attend que la mailbox0 soit vide
+  mb0->val = valeur;                                                   // On enregistre dans l'argument val, de la boîte aux lettres, la valeur lue par la photorésistance; comprise entre 0 et 1023.
+  mb0->state = FULL;                                                   // On signal que la boîte est remplie avec une nouvelle valeur pour val
+
+  if (mb2->state != EMPTY) return; 
+  mb2->val = valeur;
+  mb2->state = FULL;
+} 
+
+//------------------------------Déclaration de la tâche ISR---------------------------//
+
+struct ISR_st {
+  int timer;                                              // numéro du timer pour cette tâche utilisé par WaitFor
+  unsigned long period; 
+};
+
+struct ISR_st ISR1;
+
+void setup_ISR(struct ISR_st * ctx, int timer, unsigned long period) {
+  ctx->timer = timer;
+  ctx->period = period;
+  Serial.begin(9600);
+}
+
+
+void loop_ISR(struct ISR_st * ctx, struct mailbox1 * mb1) {
+  if (!(waitFor(ctx->timer,ctx->period))) return;
+  if(mb1->state != EMPTY)return;
+  mb1->val = LOW;
+  mb1->state = FULL;
+}
+
+
+void serialEvent() {
+    char input = (char)Serial.read();
+    if(input == 's'){
+      loop_ISR(&ISR1,&mb1);}
+   
+}
+
+
+
+//------------------------------Déclaration des tâches----------------------------------//
 
 struct Led_st Led1;
 struct Mess_st Mess1, Mess2;
 struct Oled_st Oled1;
+struct Lum_st Lum1;
 
-
-
-//--------- Setup et Loop
+//------------------------------Setup et Loop-------------------------------------------//
 
 void setup() {
-  setup_Led(&Led1, 0, 100000, 13);                        // Led est exécutée toutes les 100ms 
-  setup_Mess(&Mess1, 1, 10000000, "bonjour");              // Mess est exécutée toutes les secondes 
-  setup_Mess(&Mess2, 2, 10000000, "Salut"); 
-  setup_Oled(&Oled1, 3,1000000);
-    
+  setup_Led(&Led1, 0, 100000, 13);                    // Led1 est exécutée toutes les 100ms 
+  setup_Mess(&Mess1, 1, 1000000, "bonjour");          // Mess1 est exécutée toutes les secondes 
+  setup_Mess(&Mess2, 2, 1500000, "Salut");            // Mess2 est exécutée toutes les 1.5 secondes 
+  setup_Oled(&Oled1, 3,1000000);                      // Oled1 afficher un compteur qui s'incrémente toutes les secondes sur l'écran OLED
+  setup_Lum(&Lum1,4,500000);                          // Lum1 lit toutes les 0,5 seconde le port analogique A1 
+  setup_ISR(&ISR1,5,10000);
 }
 
 
 void loop() {
-  loop_Led(&Led1);                                        
+  loop_Led(&Led1,&mb0,&mb1);                                        
   loop_Mess(&Mess1); 
   loop_Mess(&Mess2);
-  loop_Oled(&Oled1);
-}
-
-
-void testscrolltext(void) {
-  display.clearDisplay();
-
-  display.setTextSize(2); // Draw 2X-scale text
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(10, 0);
-  display.println(F("compteur:"));
-  display.display();      // Show initial text
-  //delay(100);
-  
-  /* Scroll in various directions, pausing in-between:
-  display.startscrollright(0x00, 0x0F);
-  delay(2000);
-  display.stopscroll();
-  delay(1000);
-  display.startscrollleft(0x00, 0x0F);
-  delay(2000);
-  display.stopscroll();
-  delay(1000);
-  display.startscrolldiagright(0x00, 0x07);
-  delay(2000);
-  display.startscrolldiagleft(0x00, 0x07);
-  delay(2000);
-  display.stopscroll();
-  delay(1000);*/
-  cpt++;
-  display.print(cpt);
-  display.display();
-  display.clearDisplay();
-  display.setCursor(0,0); 
-  
+  loop_Oled(&Oled1,&mb0);
+  loop_Lum(&Lum1,&mb0,&mb2); 
+  loop_ISR(&ISR1,&mb1); 
 }
 
 
